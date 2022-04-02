d09eb87e42ebb475d299b705f560eb268f611d5d
gombi <gombos1998@gmail.com>
2022-04-02 14:01:44 +0200

    fgv , for és foreach

---
+++ kecskeKaposztafarkas/fvenyek.js
@@ -0,0 +1,52 @@
+window.addEventListener("load",init);
+
+var tomb =[];
+var tomb2 =[1,2,3];
+
+function init() {
+    var szoveg = ujfgv();
+    console.log(szoveg);
+
+    var szoveg2= function () {
+        return "haliho";
+    }
+    console.log(szoveg2);// ez egy fuggveny
+    console.log(szoveg2());//igy a függvényt meghíjuk és az eredményét iratjuk ki
+
+    var kepTomb = document.querySelectorAll("#bal>p>img");
+    //ez itt egy callback fgv 
+    kepTomb[0].addEventListener("click",function(){ //egyetlen egyszer fut le, amikor meghívjuk , nem foglal felslegesen memóriát
+        console.log(event.target.src);
+        tomb.push(event.target.src)
+
+
+    });
+
+
+    //ez itt egy nyílfügvény
+    var fgv3 =(nev) =>{
+        return "halihó" + nev;
+
+    };
+    console.log(fgv3("Jakab"));
+
+
+
+    //írjuk ki a tömb elemeit 
+    for (let i = 0; i < array.length; i++) {
+        console.log(tomb2[i]);        
+    }
+
+    //ez itt a forciklust helyettesíti, ezt írhatjuk nyílfüggvénnyel is
+    tomb2.forEach(function(elem,index){
+        console.log(elem+" "+index);
+    });
+
+}
+
+
+
+function ujfgv(){
+    return "hello";
+
+}
---
+++ kecskeKaposztafarkas/kecske.js
@@ -0,0 +1,54 @@
+window.addEventListener("load",init);
+
+function init(){
+    //console.log("Hahó")
+    var pElem = document.querySelectorAll("footer p")[0]; // rövidítés , 
+    pElem.innerHTML="Gombos Adrián"; // elem tartalmámnak megv.
+    pElem.style.textAlign="center";
+    pElem.style.fontSize="20px";
+    pElem.style.backgroundColor="lightBlue";
+
+    var kepTomb =document.querySelectorAll("#bal>p>img");
+    //console.log(kepTomb);
+
+    for (let i = 0; i < kepTomb.length; i++) {
+        
+        kepTomb[i].addEventListener("click",kattintas)
+        kepTomb[i].addEventListener("mouseover",folevisz)
+        kepTomb[i].addEventListener("mouseout",levisz)
+    }
+    
+}
+var tomb=[];
+
+
+function kattintas(){
+    
+    tomb.push(event.target.src);
+    console.log(tomb);
+    event.target.removeEventListener("click",kattintas);
+    event.target.style.display="none";
+    var aktkep="<img src='"+event.target.src+"' alt=''>";
+    document.querySelectorAll("#csonak")[0].innerHTML+=aktkep;
+
+    var uccso= document.querySelectorAll("#csonak img").length;
+
+    for (let i = 0; i < uccso; i++) {
+        
+        document.querySelectorAll("#csonak img")[i].addEventListener("mouseover",folevisz);
+        document.querySelectorAll("#csonak img")[i].addEventListener("mouseout",levisz);
+        
+    }
+
+    
+}
+
+function folevisz(){
+    //event.target.className="kiemel";
+    event.target.classList.add("kiemel");
+}
+
+function levisz(){
+    event.target.classList.remove("kiemel");
+
+}
