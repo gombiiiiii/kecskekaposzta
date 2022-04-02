fd3c1c1dc7d1bf5100e54d4ee75564b3f080f602
gombi <gombos1998@gmail.com>
2022-04-02 14:22:26 +0200

    csak egy van a csónakban

--- kecskeKaposztafarkas/kecske.js
+++ kecskeKaposztafarkas/kecske.js
@@ -3,8 +3,10 @@ window.addEventListener("load",init);
 function init(){
     //console.log("Hahó")
     var pElem = document.querySelectorAll("footer p")[0]; // rövidítés , 
+    var h1Elem = document.querySelectorAll("header h1")[0]; // rövidítés , 
     pElem.innerHTML="Gombos Adrián"; // elem tartalmámnak megv.
     pElem.style.textAlign="center";
+    h1Elem.style.textAlign="center";
     pElem.style.fontSize="20px";
     pElem.style.backgroundColor="lightBlue";
 
@@ -25,20 +27,29 @@ var tomb=[];
 function kattintas(){
     
     tomb.push(event.target.src);
+    if ((tomb.length) < 2) { //csak egy lehet a csónakban
+        
+    
     console.log(tomb);
     event.target.removeEventListener("click",kattintas);
     event.target.style.display="none";
+    
     var aktkep="<img src='"+event.target.src+"' alt=''>";
     document.querySelectorAll("#csonak")[0].innerHTML+=aktkep;
 
     var uccso= document.querySelectorAll("#csonak img").length;
 
+}
+
+    
     for (let i = 0; i < uccso; i++) {
         
         document.querySelectorAll("#csonak img")[i].addEventListener("mouseover",folevisz);
         document.querySelectorAll("#csonak img")[i].addEventListener("mouseout",levisz);
         
     }
+
+    
 
     
 }
